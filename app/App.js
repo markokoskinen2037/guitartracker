743243605f12c4b6c3ab5bb0387dd36824156392
Marko Koskinen <markokoskinen2037@gmail.com>
2018-11-23 11:20:44 +0200

    initial Commit

---
+++ app/App.js
@@ -0,0 +1,109 @@
+import React, { Component } from "react";
+import { FlatList, Text, View } from "react-native";
+import { Song } from "./components/Song";
+import firebase from "firebase";
+
+type Props = {};
+export default class App extends Component<Props> {
+    componentWillMount() {
+        var config = {
+            apiKey: "AIzaSyDTtwwDTeB_xyJd-icmoU0j2Ni7O9CNRQI",
+            authDomain: "guitartracker-3863d.firebaseapp.com",
+            databaseURL: "https://guitartracker-3863d.firebaseio.com",
+            projectId: "guitartracker-3863d",
+            storageBucket: "guitartracker-3863d.appspot.com",
+            messagingSenderId: "817606034857"
+        };
+        firebase.initializeApp(config);
+
+        console.log(firebase);
+
+        firebase
+            .database()
+            .ref("users/001")
+            .set({ name: "lol", age: 22 })
+            .then(() => {
+                console.log("testinsertion successful");
+            })
+            .catch(error => {
+                console.log("testinsertion caused error " + error);
+            });
+    }
+
+    render() {
+        return (
+            <View
+                style={{
+                    backgroundColor: "#1a1a1a" //Hiilenharmaa tausta
+                }}>
+                <View
+                    style={{ backgroundColor: "white", flexDirection: "row" }}>
+                    <Text
+                        style={{
+                            fontSize: 20,
+                            padding: 20,
+                            backgroundColor: "white",
+                            flex: 4
+                        }}>
+                        🎸tracker 2018 v.0.3
+                    </Text>
+                    <Text
+                        style={{
+                            backgroundColor: "white",
+                            flex: 1,
+                            textAlign: "right",
+                            marginTop: 5,
+                            marginRight: 5
+                        }}
+                        onPress={() => alert("TODO :)")}>
+                        Profile
+                    </Text>
+                </View>
+                <View style={{ marginTop: 15 }}>
+                    <FlatList
+                        data={[
+                            { name: "Melissa", hardness: 5, mastered: false },
+                            {
+                                name: "Darude sandstorm",
+                                hardness: 5,
+                                mastered: true
+                            },
+                            {
+                                name: "Killing time",
+                                hardness: 3,
+                                mastered: false
+                            },
+                            {
+                                name: "In the name of GOD",
+                                hardness: 5,
+                                mastered: false
+                            },
+                            {
+                                name: "Freedom to Mars",
+                                hardness: 2,
+                                mastered: true
+                            },
+                            {
+                                name: "For the lord",
+                                hardness: 5,
+                                mastered: false
+                            },
+                            {
+                                name: "Asians in japan",
+                                hardness: 5,
+                                mastered: false
+                            },
+                            { name: "Kill bill", hardness: 5, mastered: true },
+                            {
+                                name: "Bae bae bam",
+                                hardness: 5,
+                                mastered: false
+                            }
+                        ]}
+                        renderItem={({ item }) => <Song song={item} />}
+                    />
+                </View>
+            </View>
+        );
+    }
+}
---
+++ app/components/Song.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+
+import { View, Text } from "react-native";
+
+export class Song extends Component {
+    render() {
+        return (
+            <View
+                style={{
+                    flex: 1,
+                    padding: 7,
+                    marginHorizontal: 10,
+                    marginVertical: 5,
+                    flexDirection: "row",
+                    borderRadius: 5,
+                    backgroundColor: "white"
+                }}>
+                <View
+                    style={{
+                        flex: 1,
+                        backgroundColor: "lightgray",
+                        marginRight: 10,
+                        height: 65
+                    }}>
+                    <Text
+                        onPress={() => alert("TODO :)")}
+                        style={{
+                            flex: 1,
+                            textAlignVertical: "center",
+                            textAlign: "center"
+                        }}>
+                        PLAY
+                    </Text>
+                </View>
+                <View style={{ flex: 2 }}>
+                    <Text>
+                        <Text style={{ fontWeight: "bold" }}>Name: </Text>
+                        {this.props.song.name}
+                    </Text>
+                    <Text>
+                        <Text style={{ fontWeight: "bold" }}>Hardness: </Text>
+                        {this.props.song.hardness}
+                    </Text>
+
+                    {this.props.song.mastered ? (
+                        <Text style={{ fontWeight: "bold" }}>
+                            Mastered: 👍{" "}
+                        </Text>
+                    ) : (
+                        <Text style={{ fontWeight: "bold" }}>
+                            Mastered: 👎{" "}
+                        </Text>
+                    )}
+                </View>
+            </View>
+        );
+    }
+}
+
+export default Song;
