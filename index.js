e8f9776552a3383a65c1abe9af251ca55df1897a
Marko Koskinen <markokoskinen2037@gmail.com>
2019-01-18 14:55:14 +0200

    bu

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 import { AppRegistry } from "react-native";
 import App from "./app/App";
 import { name as appName } from "./app.json";
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import React from "react";
+
+class HomeScreen extends React.Component {
+    render() {
+        return (
+            <View
+                style={{
+                    flex: 1,
+                    alignItems: "center",
+                    justifyContent: "center"
+                }}>
+                <Text>Home Screen</Text>
+            </View>
+        );
+    }
+}
+
+const AppNavigator = createStackNavigator({
+    Home: {
+        screen: HomeScreen
+    }
+});
 
 AppRegistry.registerComponent(appName, () => App);
